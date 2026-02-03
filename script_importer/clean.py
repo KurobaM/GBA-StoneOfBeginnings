5f1830e3e4a313dfc6a6895025cd38efe0fa248d
KurobaM <kuroba.m17@gmail.com>
2026-02-03 15:23:07 +0800

    cleanup trailing space

--- script_importer/clean.py
+++ script_importer/clean.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
         for file in files:
             if file.split('.')[-1] == 'txt':
                 paths.append(os.path.join(root, file))
-                
+
     for fn in paths:
-        with open(fn, 'r',encoding='utf-8') as f:
+        with open(fn, 'r', encoding='utf-8') as f:
             data = f.read()
         cleaned = re.sub(r'(\r\n|\n)(\r\n|\n)+', '\n\n', data)
+        cleaned = "\n".join(line.rstrip() for line in cleaned.splitlines())
         with open(fn, 'w', encoding='utf-8') as f:
             f.write(cleaned)
