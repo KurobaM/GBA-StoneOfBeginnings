a0d72a3f2134a6e6bff78b63db639680d4de6268
KurobaM <kuroba.m17@gmail.com>
2026-02-16 21:26:39 +0800

    using system_message directly from SNSC3-Translation

--- build_stage/system_message.bat
+++ build_stage/system_message.bat
@@ -1,3 +1,3 @@
 ::SYSTEM MESSAGES
-script_inserter\swordcraft3-menu build\swordcraft3-test.gba system_messages\magic.txt system_messages\weapons.txt system_messages\link.txt system_messages\effects.txt system_messages\special_attacks.txt system_messages\dictionary.txt system_messages\items.txt system_messages\menu.txt system_messages\bonus.txt system_messages\menu3.txt
-::script_inserter\swordcraft3-menu build\swordcraft3-test.gba system_messages\menu3.txt
+script_inserter\swordcraft3-menu build\swordcraft3-test.gba script\system_messages\magic.txt script\system_messages\weapons.txt script\system_messages\link.txt script\system_messages\effects.txt script\system_messages\special_attacks.txt script\system_messages\dictionary.txt script\system_messages\items.txt script\system_messages\menu.txt script\system_messages\bonus.txt script\system_messages\menu3.txt
+::script_inserter\swordcraft3-menu build\swordcraft3-test.gba script\system_messages\menu3.txt
