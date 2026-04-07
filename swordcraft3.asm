56b3b83186e1943e50170deb83b59d0de20350f8
KurobaM <kuroba.m17@gmail.com>
2026-04-07 16:04:09 +0800

    Build v3.0 (debug), Build v3.0 (release)

--- swordcraft3.asm
+++ swordcraft3.asm
@@ -15,7 +15,6 @@
 .include "asm/vwf_sprite_renderer.asm"
 .include "asm/vwf_sprites.asm"
 .include "asm/name_portrait_check.asm"
-.include "asm/names_obj.asm"
 
 ; Reduces the size of scripts so we don't have to worry about space much.
 .include "asm/script_expr.asm"
@@ -69,9 +68,9 @@
 ;graphics
 ; title
 .org 0x095AE3AC
-.import "graphic/title.cg8"
+.import "graphic/title.95ae3ac.cg8"
 .org 0x095B34DC
-.import "graphic/title.sc8"
+.import "graphic/title.95b34dc.sc8"
 
 ;title options
 .include "asm/gfx_title_2player.asm"
@@ -81,18 +80,18 @@
 
 ; craft sword text
 .org 0x09593A8C
-.import "graphic/craft_sword.cg4"
+.import "graphic/craft_sword.9593a8c.cg4"
 .org 0x0959479C
-.import "graphic/craft_sword.sc4"
+.import "graphic/craft_sword.959479c.sc4"
 
 .org 0x0958632C
-.import "graphic/lyndbaum_text_1.cg4"
+.import "graphic/lyndbaum_text_1.958632c.cg4"
 .org 0x09586C1C
-.import "graphic/lyndbaum_text_1.sc4"
+.import "graphic/lyndbaum_text_1.9586c1c.sc4"
 .org 0x095870EC
-.import "graphic/lyndbaum_text_2.cg4"
+.import "graphic/lyndbaum_text_2.95870ec.cg4"
 .org 0x0958791C
-.import "graphic/lyndbaum_text_2.sc4"
+.import "graphic/lyndbaum_text_2.958791c.sc4"
 
 .org 0x954458C
 .import "asm/credits_craftsword_tile.lzss"
@@ -111,25 +110,25 @@
 
 ;fishing_minigame
 .org 0x09646E2C
-.import "graphic/fishing.cg4.lz77"
+.import "graphic/fishing.9646e2c.cg4.lz77"
 .org 0x0964684C
-.import "graphic/fishing.sc4.lz77"
+.import "graphic/fishing.964684c.sc4.lz77"
 
 ;link
 .org 0x0952891C
-.import "graphic/s_tsuushin_k1.cg4.lz77"
+.import "graphic/s_tsuushin_k1.952891c.cg4.lz77"
 .org 0x0953682C
-.import "graphic/s_tsuushin_k1.sc4.lz77"
+.import "graphic/s_tsuushin_k1.953682c.sc4.lz77"
 ;s_omake
 .org 0x0952A5EC
-.import "graphic/s_omake_k1.cg4.lz77"
+.import "graphic/s_omake_k1.952a5ec.cg4.lz77"
 .org 0x09536DEC
-.import "graphic/s_omake_k1.sc4.lz77"
+.import "graphic/s_omake_k1.9536dec.sc4.lz77"
 ;bestiary
 .org 0x094FF70C
-.import "graphic/bestiary_k1.cg4.lz77"
+.import "graphic/bestiary_k1.94ff70c.cg4.lz77"
 .org 0x0952F9EC
-.import "graphic/bestiary_k1.sc4.lz77"
+.import "graphic/bestiary_k1.952f9ec.sc4.lz77"
 
 
 ;lottery minigame gfx
@@ -138,9 +137,9 @@
 
 ;firewood minigame gfx
 .org 0x094CF11C
-.import "graphic/firewood_k1_tile.bin"
+.import "graphic/firewood_k1.94cf11c.cg4"
 .org 0x094D3C4C
-.import "graphic/firewood_k1_map.bin"
+.import "graphic/firewood_k1.94d3c4c.sc4"
 
 ;minigame results
 .org 0x964F58C
@@ -154,25 +153,139 @@
 .org 0x9650F1C
 .import "asm/lottery/5_place.lzss"
 .org 0x0965513C
-.import "graphic/lottery_rules_k1.cg4.lz77"
+.import "graphic/lottery_rules_k1.965513c.cg4.lz77"
 .org 0x0965827C
-.import "graphic/lottery_rules_k1.sc4.lz77"
+.import "graphic/lottery_rules_k1.965827c.sc4.lz77"
 .org 0x964A99C
-.import "graphic/lottery_scr_k1.cg4.lz77"
+.import "graphic/lottery_scr_k1.964a99c.cg4.lz77"
 .org 0x0964B9AC
-.import "graphic/lottery_scr_k1.sc4.lz77"
+.import "graphic/lottery_scr_k1.964b9ac.sc4.lz77"
 .include "asm/lottery_ticket.asm"
 
 ;battle messages (guard, poison, sleep)
 .include "asm/guard.asm"
 
-;customize screen
-.org 0x0951237C
-.import "graphic/s_custom_1_k1_tile.lzss"
-.org 0x09532E0C
-.import "graphic/s_custom_1_k1_map.lzss"
-
 ;other messages
 .include "asm/non_psi3_script_text.asm"
+
+;customize screen
+.org 0x0951237C
+.import "graphic/s_custom_1_k1.951237c.cg4.lz77"
+.org 0x09532E0C
+.import "graphic/s_custom_1_k1.9532e0c.sc4.lz77"
+
+; name tag
+.org 0x9dbed8c
+.import "graphic/name_tag/100.9dbed8c.cg4"
+.org 0x9dbefec
+.import "graphic/name_tag/101.9dbefec.cg4"
+.org 0x9dbf24c
+.import "graphic/name_tag/102.9dbf24c.cg4"
+.org 0x9dbf4ac
+.import "graphic/name_tag/103.9dbf4ac.cg4"
+.org 0x9dbf70c
+.import "graphic/name_tag/104.9dbf70c.cg4"
+.org 0x9dbf96c
+.import "graphic/name_tag/105.9dbf96c.cg4"
+.org 0x9dbfbcc
+.import "graphic/name_tag/106.9dbfbcc.cg4"
+.org 0x9dbfe2c
+.import "graphic/name_tag/107.9dbfe2c.cg4"
+.org 0x9dc008c
+.import "graphic/name_tag/108.9dc008c.cg4"
+.org 0x9dc02ec
+.import "graphic/name_tag/109.9dc02ec.cg4"
+.org 0x9dc054c
+.import "graphic/name_tag/110.9dc054c.cg4"
+.org 0x9dc07ac
+.import "graphic/name_tag/111.9dc07ac.cg4"
+.org 0x9dc0a0c
+.import "graphic/name_tag/112.9dc0a0c.cg4"
+.org 0x9dc0c6c
+.import "graphic/name_tag/113.9dc0c6c.cg4"
+.org 0x9dc0ecc
+.import "graphic/name_tag/114.9dc0ecc.cg4"
+.org 0x9dc112c
+.import "graphic/name_tag/115.9dc112c.cg4"
+.org 0x9dc138c
+.import "graphic/name_tag/116.9dc138c.cg4"
+.org 0x9dc15ec
+.import "graphic/name_tag/117.9dc15ec.cg4"
+.org 0x9dc184c
+.import "graphic/name_tag/118.9dc184c.cg4"
+.org 0x9dc1aac
+.import "graphic/name_tag/119.9dc1aac.cg4"
+.org 0x9dc1d0c
+.import "graphic/name_tag/120.9dc1d0c.cg4"
+.org 0x9dc1f6c
+.import "graphic/name_tag/121.9dc1f6c.cg4"
+.org 0x9dc21cc
+.import "graphic/name_tag/122.9dc21cc.cg4"
+.org 0x9dc242c
+.import "graphic/name_tag/123.9dc242c.cg4"
+.org 0x9dc268c
+.import "graphic/name_tag/124.9dc268c.cg4"
+.org 0x9dc28ec
+.import "graphic/name_tag/125.9dc28ec.cg4"
+.org 0x9dc2b4c
+.import "graphic/name_tag/126.9dc2b4c.cg4"
+.org 0x9dc2dac
+.import "graphic/name_tag/127.9dc2dac.cg4"
+.org 0x9dc300c
+.import "graphic/name_tag/128.9dc300c.cg4"
+.org 0x9dc326c
+.import "graphic/name_tag/129.9dc326c.cg4"
+.org 0x9dc34cc
+.import "graphic/name_tag/130.9dc34cc.cg4"
+.org 0x9dc372c
+.import "graphic/name_tag/131.9dc372c.cg4"
+.org 0x9dc398c
+.import "graphic/name_tag/132.9dc398c.cg4"
+.org 0x9dc3bec
+.import "graphic/name_tag/133.9dc3bec.cg4"
+.org 0x9dc3e4c
+.import "graphic/name_tag/134.9dc3e4c.cg4"
+.org 0x9dc40ac
+.import "graphic/name_tag/135.9dc40ac.cg4"
+.org 0x9dc430c
+.import "graphic/name_tag/136.9dc430c.cg4"
+.org 0x9dc456c
+.import "graphic/name_tag/137.9dc456c.cg4"
+.org 0x9dc47cc
+.import "graphic/name_tag/138.9dc47cc.cg4"
+.org 0x9dc4a2c
+.import "graphic/name_tag/139.9dc4a2c.cg4"
+.org 0x9dc4c8c
+.import "graphic/name_tag/140.9dc4c8c.cg4"
+.org 0x9dc4eec
+.import "graphic/name_tag/141.9dc4eec.cg4"
+.org 0x9dc514c
+.import "graphic/name_tag/142.9dc514c.cg4"
+.org 0x9dc53ac
+.import "graphic/name_tag/143.9dc53ac.cg4"
+.org 0x9dc560c
+.import "graphic/name_tag/144.9dc560c.cg4"
+.org 0x9dc586c
+.import "graphic/name_tag/145.9dc586c.cg4"
+.org 0x9dc5acc
+.import "graphic/name_tag/146.9dc5acc.cg4"
+.org 0x9dc5d2c
+.import "graphic/name_tag/147.9dc5d2c.cg4"
+.org 0x9dc5f8c
+.import "graphic/name_tag/148.9dc5f8c.cg4"
+.org 0x9dc61ec
+.import "graphic/name_tag/149.9dc61ec.cg4"
+.org 0x9dc644c
+.import "graphic/name_tag/150.9dc644c.cg4"
+.org 0x9dc66ac
+.import "graphic/name_tag/151.9dc66ac.cg4"
+.org 0x9dc690c
+.import "graphic/name_tag/152.9dc690c.cg4"
+.org 0x9dc6b6c
+.import "graphic/name_tag/153.9dc6b6c.cg4"
+.org 0x9dc6dcc
+.import "graphic/name_tag/154.9dc6dcc.cg4"
+.org 0x9dc702c
+.import "graphic/name_tag/155.9dc702c.cg4"
 
 .close
